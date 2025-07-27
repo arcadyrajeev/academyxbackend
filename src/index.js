5342b67cae382d00d46a39262861a0de7c5c39d3
arcadyrajeev <bitmasteratj@gmail.com>
2025-07-27 10:37:38 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-require('dotenv').config()
-const app = require("./app.js")
-const connectDB = require('./db/index.js')
+require("dotenv").config();
 
-const port = process.env.PORT || 8000
+const app = require("./app.js");
+const connectDB = require("./db/index.js");
+
+const port = process.env.PORT || 8000;
 connectDB()
-.then(()=>{
-    app.listen(port,()=>{
-        console.log(`server start at ${port}`)
-    })
-    app.on("error",(err)=>{
-        console.log("your app is not running",err)
-        throw err
-    })
-})
-.catch((err)=>{
-    console.log("mongodb connection failed",err)
-})
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server start at ${port}`);
+    });
+    app.on("error", (err) => {
+      console.log("your app is not running", err);
+      throw err;
+    });
+  })
+  .catch((err) => {
+    console.log("mongodb connection failed", err);
+  });
